33c2ab6ba44d9e7d8057c67dc5b0c7cb3410db2f
Vishal Kotekar <vishalkotekar0096@gmail.com>
2024-10-20 16:47:47 +0530

    added file

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345678
+let accountName = "Vishal"
+var city = "ichalkaranji"
+let accountPass
+
+// accountId = 4444
+console.log(accountId)
+console.table([accountId,accountName,city,accountPass])
