90a7c57bdcad21862d1db70e95a3dc97f1adb60d
Vishal Kotekar <vishalkotekar0096@gmail.com>
2024-10-20 16:49:19 +0530

    added datataype

---
+++ 02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";//strict method
+
+let check = null;
+let key;
+console.log(typeof check)
+console.log(typeof key)
+
+//primitive datatypes
+
+/*
+1.String
+2.number
+3.undefined
+4.boolean
+5.bigint
+6.null = standalone value type of null is object
+7.symobol
+*/ 
+
+// nonprimitive datatype
+//object
